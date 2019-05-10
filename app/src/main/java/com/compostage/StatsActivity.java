5e25a0fbf92a970d9cda87cc8ecee0cc6c16264c
jess7979 <theoneandtheonlyjess@gmail.com>
2019-05-10 12:32:26 -0400

    ajout de Menu navigation

--- app/src/main/java/com/compostage/StatsActivity.java
+++ app/src/main/java/com/compostage/StatsActivity.java
@@ -1,12 +1,16 @@
 package com.compostage;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -24,6 +28,35 @@ public class StatsActivity extends AppCompatActivity {
         zone.clear();
         bed.clear();
         listSpinner.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.navMenu:
+            {
+                Intent in = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(in);
+                this.finish();
+                break;
+            }
+            case R.id.navStats:
+            {
+                Intent in = new Intent(getBaseContext(), StatsActivity.class);
+                startActivity(in);
+                this.finish();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void loadZone(){
