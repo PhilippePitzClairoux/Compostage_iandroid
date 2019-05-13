cbf83a1acd0f19877b5d3be187f138ac7261199f
x <philippepitz123@gmail.com>
2019-05-13 12:38:56 +0000

    added userType!

--- app/src/main/java/com/compostage/ServerQueries.java
+++ app/src/main/java/com/compostage/ServerQueries.java
@@ -4,7 +4,7 @@ class ServerQueries {
 
     private static final String HOST = "192.168.0.102";
 
-    static final String VALIDATE_USERNAME = "http://" + HOST +
+    static final String GET_USER_INFO = "http://" + HOST +
             "/controller/java/getUserInfo.php?username=%s";
 
     static final String VALIDATE_PASSWORD = "http://" + HOST +
@@ -15,5 +15,10 @@ class ServerQueries {
 
     static final String SEND_MEASUREMENT_DATA = "http://" + HOST +
             "/controller/java/sendMeasurementData.php?sensor_id=%i&value=%f&type_id=%i&";
+
+
+    //Note : user_auth_answer will be encrypted during this request
+    static final String SYNC_USER_INFO = "http://" + HOST +
+            "/controller/java/updateUserInfo.php?username=%s&user_password=%s&user_email=%s&user_auth_question=%s&user_auth_answer=%s";
 
 }
