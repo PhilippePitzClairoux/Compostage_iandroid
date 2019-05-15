e98e675054aaee89e7b3e6e3d9d89a62a4f9a5eb
DESKTOP-OHJLOHD\TheTh <thethousandbeanster@hotmail.com>
2019-05-15 15:16:06 -0400

    Can create data

--- app/src/main/java/com/compostage/SensorMeasure.java
+++ app/src/main/java/com/compostage/SensorMeasure.java
@@ -1,5 +1,8 @@
 package com.compostage;
 
+import android.database.Cursor;
+import android.widget.Toast;
+
 public class SensorMeasure {
     public SensorMeasure(){
         this.setValue(null);
@@ -11,9 +14,10 @@ public class SensorMeasure {
         this.setTimeStamp("");
     }
 
-    public SensorMeasure(float value, String timeStamp){
+    public SensorMeasure(float value, String timeStamp, String type){
         this.setValue(value);
         this.setTimeStamp(timeStamp);
+        this.setType(type);
     }
 
     public SensorMeasure(int id, float value, String timeStamp){
@@ -25,6 +29,7 @@ public class SensorMeasure {
     private int id;
     private Float value;
     private String timeStamp;
+    private String type;
 
     public int getId() {
         return id;
@@ -48,5 +53,29 @@ public class SensorMeasure {
 
     public void setTimeStamp(String timeStamp) {
         this.timeStamp = timeStamp;
+    }
+
+    public String getType(){return type;}
+
+    public void setType(String type){this.type=type;}
+
+    public void addToDatabase(db_query_engine db, int sensorId){
+
+
+        String query = "SELECT measure_id FROM measures WHERE sensor_id = "+sensorId+" AND measure_timestamp = '"+timeStamp+"'";
+
+        Cursor cursor = db.execution_with_return(query);
+
+        int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex("measure_id")));
+
+        query = "SELECT measure_type_id FROM measure_type WHERE measure_type_name = '"+type+"'";
+
+        cursor = db.execution_with_return(query);
+
+        int typeId=Integer.parseInt(cursor.getString(cursor.getColumnIndex("measure_type_id")));
+
+        query = "INSERT INTO ta_measure_type (measure_id, measure_type_id, measure_value) VALUES ("+id+","+typeId+","+value+")";
+
+        db.execution(query);
     }
 }
