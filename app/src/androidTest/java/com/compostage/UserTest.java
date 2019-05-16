d2d8326427fe5fff450f95170e937e00f8eb7a99
x <philippepitz123@gmail.com>
2019-05-16 11:21:52 +0000

    JUINIT

---
+++ app/src/androidTest/java/com/compostage/UserTest.java
@@ -0,0 +1,57 @@
+package com.compostage;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.compostage.Data.User;
+import com.compostage.Exceptions.InvalidServerQuery;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class UserTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void fetch_data_locally() {
+
+        db_query_engine engine = new db_query_engine(InstrumentationRegistry.getTargetContext());
+        User tmp = new User("admin", engine);
+
+        tmp.fetch_data_locally();
+        assertTrue(tmp.is_loaded_properly());
+
+    }
+
+    //we check for this because in order for this feature to actually work,
+    // we need alot of configuration
+    @Test(expected = IOException.class)
+    public void fetch_data_remotely() throws InvalidServerQuery, IOException {
+
+        db_query_engine engine = new db_query_engine(InstrumentationRegistry.getTargetContext());
+        User tmp = new User("admin", engine);
+
+        tmp.fetch_data();
+        assertTrue(tmp.is_loaded_properly());
+    }
+
+    @Test
+    public void test_password() {
+
+        User tmp = new User("test", null);
+
+        tmp.setPassword("test");
+        assertTrue(tmp.test_password("test"));
+    }
+
+}
