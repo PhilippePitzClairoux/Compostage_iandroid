158ab36164ffbc4a2e4841e81278a10e84e02fae
x <philippepitz123@gmail.com>
2019-05-16 11:31:04 +0000

    FIXED STUFF

--- app/src/androidTest/java/com/compostage/UserTest.java
+++ app/src/androidTest/java/com/compostage/UserTest.java
@@ -13,6 +13,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 
 import static org.junit.Assert.*;
 
@@ -35,14 +36,13 @@ public class UserTest {
 
     //we check for this because in order for this feature to actually work,
     // we need alot of configuration
-    @Test(expected = IOException.class)
-    public void fetch_data_remotely() throws InvalidServerQuery, IOException {
+    @Test(expected = UnknownHostException.class)
+    public void fetch_data_remotely() throws IOException, InvalidServerQuery {
 
         db_query_engine engine = new db_query_engine(InstrumentationRegistry.getTargetContext());
         User tmp = new User("admin", engine);
 
         tmp.fetch_data();
-        assertTrue(tmp.is_loaded_properly());
     }
 
     @Test
